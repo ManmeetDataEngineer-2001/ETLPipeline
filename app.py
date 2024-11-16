cd76aa217f6021efcfc8733ba41a7f7ae67c78c8
ManmeetDataEngineer-2001 <manmeet02012001@gmail.com>
2024-11-16 22:46:36 +0530

    My first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import sys
+from config import DB_DETAILS
+def main():
+    env=sys.argv[1]
+    db_details=DB_DETAILS[env]
+    print(db_details)
+    print("Hello World")
+
+if __name__=='__main__':
+    main()
